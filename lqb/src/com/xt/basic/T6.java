6c2522c668f91892df3d704e9a96cc6115a5c340
WangMin <1426180565@qq.com>
2018-03-24 12:23:20 +0800

    杨辉三角

--- lqb/src/com/xt/basic/T6.java
+++ lqb/src/com/xt/basic/T6.java
@@ -4,14 +4,20 @@ import java.util.Scanner;
 
 public class T6 {
 	public static void main(String[] args) {
-//		Scanner sc=new Scanner(System.in);
-//		int n=sc.nextInt();
-		for(int i=1;i<=3;i++) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[][]=new int[n][n];
+		for(int i=1;i<=n;i++) {
 			for(int j=1;j<=i;j++) {
-				System.out.print(i);
+				if(j!=1&&j!=n) {
+					a[i][j]=a[i-1][j-1]+a[i-1][j];
+				}
 			}
-			
+			System.out.println();
 		}
+		
 	}
+	
 
 }
+
