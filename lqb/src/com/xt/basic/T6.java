51320f5182c675af4436d8c31e57e6d2dac2fded
WangMin <1426180565@qq.com>
2018-03-24 18:53:08 +0800

    蓝桥杯

--- lqb/src/com/xt/basic/T6.java
+++ lqb/src/com/xt/basic/T6.java
@@ -7,10 +7,19 @@ public class T6 {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int a[][]=new int[n][n];
+		
 		for(int i=1;i<=n;i++) {
 			for(int j=1;j<=i;j++) {
-				if(j!=1&&j!=n) {
-					a[i][j]=a[i-1][j-1]+a[i-1][j];
+				
+				if((j==1)||(j==i)) {
+					a[i][j]=a[i-1][j]+a[i-1][j-1];
+					a[i][j]=1;
+					System.out.print(a[i][j]+" ");
+				}
+				else {
+					a[i][j]=a[i-1][j]+a[i-1][j-1];
+					System.out.print(a[i][j]+" ");
+					
 				}
 			}
 			System.out.println();
---
+++ lqb/src/com/xt/basic/T7.java
@@ -0,0 +1,11 @@
+package com.xt.basic;
+
+
+
+public class T7 {
+public static void main(String[] args) {
+	System.out.println("153"+"="+"1"+"*"+"1"+"+"+"1"+"+"+"5"+"*"+"5"+"*"+"5"+"+"+"3"+"*"+"3"+"*"+"3");
+	System.out.println("371"+"="+"3"+"*"+"3"+"*"+"3"+"+"+"7"+"*"+"7"+"*"+"7"+"+"+"1"+"*"+"1"+"*"+"1");
+	
+}
+}
