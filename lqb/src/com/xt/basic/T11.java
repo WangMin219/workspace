18d361b2b1abed06faa0b3748092e82f3bbcb056
WangMin <1426180565@qq.com>
2018-03-25 17:46:43 +0800

    蓝桥杯

--- lqb/src/com/xt/basic/T11.java
+++ lqb/src/com/xt/basic/T11.java
@@ -2,12 +2,12 @@ package com.xt.basic;
 
 import java.util.Scanner;
 
-public class T10 {
+public class T11 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		Integer c=sc.nextInt();
-		System.out.println(c.toHexString(c));
-		
+		Integer c=sc.nextInt(16);
+		System.out.println(c);
 	}
+	
 
 }
--- lqb/src/com/xt/basic/T12.java
+++ lqb/src/com/xt/basic/T12.java
@@ -2,12 +2,11 @@ package com.xt.basic;
 
 import java.util.Scanner;
 
-public class T10 {
+public class T12 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		Integer c=sc.nextInt();
-		System.out.println(c.toHexString(c));
-		
+		Integer c=sc.nextInt(16);
+		System.out.println(c.toOctalString(c));
 	}
 
 }
