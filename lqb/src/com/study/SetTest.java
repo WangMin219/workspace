85db3adec9cd8518b6c53ef954e92cc898989b7b
WangMin <1426180565@qq.com>
2018-03-25 15:23:18 +0800

    Set和Interger

--- lqb/src/com/study/SetTest.java
+++ lqb/src/com/study/SetTest.java
@@ -7,7 +7,7 @@ public class SetTest {
 	public static void main(String[] args) {
 		int a[]= {1,2,3,4,5,1,2,3,4};
 		Set set=new TreeSet();
-		set.put(a);
+		
 		for(int i=0;i<set.size();i++) {
 			System.out.println(a[i]);
 		}
