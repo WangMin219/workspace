a7b6751efc62a5d1bfb5469fc3aed98fb6ec9e02
WangMin <1426180565@qq.com>
2018-03-27 23:28:43 +0800

    蓝桥杯

--- lqb/src/com/s8/T3.java
+++ lqb/src/com/s8/T3.java
@@ -2,35 +2,64 @@ package com.s8;
 
     public class T3 {  
         public static void main(String[] args) {  
+        	int count=0;
+        	for(int A=1;A<9;A++) {
+        		for(int B=1;B<9;B++) {
+        			for(int C=1;C<9;C++) {
+        				for(int D=1;D<9;D++) {
+        	        		for(int E=1;E<9;E++) {
+        	        			for(int F=1;F<9;F++) {
+        	        				for(int G=1;G<9;G++) {
+        	        	        		for(int H=1;H<9;H++) {
+        	        	        			for(int I=1;I<9;I++) {
+        	        	        				
+        	        	        				
+        	        	        			}
+        	        	        			
+        	        	        		}
+        	        	        		
+        	        	        	}
+        	        				
+        	        			}
+        	        			
+        	        		}
+        	        		
+        	        	}
+        				
+        			}
+        			
+        		}
+        		
+        	}
             // TODO Auto-generated method stub  
-            int count = 0;  
-            for (int i = 1; i < 10; i++) {  
-                for (int j = 1; j < 10; j++) {  
-                    for (int k = 1; k < 10; k++) {  
-                        for (int l = 1; l < 10; l++) {  
-                            for (int m = 1; m < 10; m++) {  
-                                for (int n = 1; n < 10; n++) {  
-                                    for (int o = 1; o < 10; o++) {  
-                                        for (int p = 1; p < 10; p++) {  
-                                            for (int q = 1; q < 10; q++) {  
-                                                if ((i + j + k + l) == (l + m + n + o) && (i + j + k + l) == (o + q + p + i)  
-                                                        && i != j && i != k && i != l && i != m && i != n && i != o  
-                                                        && i != p && i != q && j != k && j != l && j != m && j != n  
-                                                        && j != o && j != p && j != q && k != l && k != m && k != n  
-                                                        && k != o && k != p && k != q && l != m && l != n && l != o  
-                                                        && l != p && l != q && m != n && m != o && m != p && m != q  
-                                                        && n != o && n != p && n != q && o != p && o != q && p != q) {  
-                                                    count++;  
-                                                }  
-                                            }  
-                                        }  
-                                    }  
-                                }  
-                            }  
-                        }  
-                    }  
-                }  
-            }  
-            System.out.println(count / 6);// 旋转是三种相同的，镜像也是三种相同的。所以除以6  
+//            int count = 0;  
+//            for (int i = 1; i < 10; i++) {  
+//                for (int j = 1; j < 10; j++) {  
+//                    for (int k = 1; k < 10; k++) {  
+//                        for (int l = 1; l < 10; l++) {  
+//                            for (int m = 1; m < 10; m++) {  
+//                                for (int n = 1; n < 10; n++) {  
+//                                    for (int o = 1; o < 10; o++) {  
+//                                        for (int p = 1; p < 10; p++) {  
+//                                            for (int q = 1; q < 10; q++) {  
+//                                                if ((i + j + k + l) == (l + m + n + o) && (i + j + k + l) == (o + q + p + i)  
+//                                                        && i != j && i != k && i != l && i != m && i != n && i != o  
+//                                                        && i != p && i != q && j != k && j != l && j != m && j != n  
+//                                                        && j != o && j != p && j != q && k != l && k != m && k != n  
+//                                                        && k != o && k != p && k != q && l != m && l != n && l != o  
+//                                                        && l != p && l != q && m != n && m != o && m != p && m != q  
+//                                                        && n != o && n != p && n != q && o != p && o != q && p != q) {  
+//                                                    count++;  
+//                                                }  
+//                                            }  
+//                                        }  
+//                                    }  
+//                                }  
+//                            }  
+//                        }  
+//                    }  
+//                }  
+//            }  
+//            System.out.println(count / 6);// 旋转是三种相同的，镜像也是三种相同的。所以除以6  
         }  
     }  
