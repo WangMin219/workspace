4ebb281fa1cb7d198535f4ba6cfd42ff5945da96
WangMin <1426180565@qq.com>
2018-04-11 14:40:36 +0800

    springmvc

---
+++ day04_11/src/com/vo/Hello.java
@@ -0,0 +1,19 @@
+package com.vo;
+
+public class Hello {
+	private String msg;
+
+	public Hello() {
+		super();
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+
+}
---
+++ day04_11/src/com/vo/Test.java
@@ -0,0 +1,21 @@
+package com.vo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	public static void main(String[] args) {
+//		ApplicationContext axt=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+//		Hello hello=(Hello) axt.getBean("hello");
+//		System.out.println(hello.getMsg());
+		
+		ApplicationContext axt=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Course course=(Course) axt.getBean("course");
+		System.out.println(course);
+		ApplicationContext axt1=new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		Course course1=(Course) axt.getBean("course1");
+		System.out.println(course1);
+	}
+
+}
+                                                                                                                                                                                 
